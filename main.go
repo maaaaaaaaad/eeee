173e28dc0b7ca8c7bcbd14d321cf99d548357ba3
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-20 04:52:25 +0000

    e

--- main.go
+++ main.go
@@ -7,21 +7,16 @@ import (
 
 func main() {
 	r := 5.0
-	h := 10.0
 	n := 1000
-
-	l := math.Sqrt(r*r + h*h)
+	a := 0.0
+	b := 2.0 * math.Pi * r
+	dx := (b - a) / float64(n)
 
 	f := func(x float64) float64 {
-		rx := (r * (h - x)) / h
-		ax := math.Pi * rx * rx
-		vx := (1.0 / 3.0) * ax * (h - x)
-		return vx
+		ax := math.Pi * (r*r - x*x)
+		return ax
 	}
 
-	a := 0.0
-	b := h
-	dx := (b - a) / float64(n)
 	sum := (f(a) + f(b)) / 2.0
 	for i := 1; i < n; i++ {
 		x := a + float64(i)*dx
@@ -29,7 +24,8 @@ func main() {
 	}
 	integral := dx * sum
 
-	volume := integral * l / 3.0
+	volume := 4.0 / 3.0 * math.Pi * math.Pow(r, 3.0)
+	volume = volume * integral / (math.Pi * math.Pow(r, 2.0))
 
-	fmt.Printf("The volume of the cone is %.2f\n", volume)
+	fmt.Printf(" %.2f\n", volume)
 }
