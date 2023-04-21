a6947308b9891cf0c2db7be7919f4f569b2c09b8
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-21 05:25:59 +0000

    e

--- main.go
+++ main.go
@@ -4,40 +4,76 @@ import (
 	"fmt"
 )
 
-func solution(sequence []int, k int) []int {
-	n := len(sequence)
-	var ans []int
-	i, j := 0, 1
-	sum := sequence[i] + sequence[j]
-	minLen := n + 1
-	for i < n {
-		if sum == k {
-			if j-i+1 < minLen {
-				ans = []int{i, j}
-				minLen = j - i + 1
-			}
-			j++
-			if j == n {
-				break
-			}
-			sum += sequence[j]
-		} else if sum < k {
-			j++
-			if j == n {
-				break
-			}
-			sum += sequence[j]
+type Node struct {
+	value    int
+	children []*Node
+	parent   *Node
+}
+
+func newNode(value int) *Node {
+	return &Node{
+		value:    value,
+		children: make([]*Node, 0),
+	}
+}
+
+func (n *Node) querySum() int {
+	sum := n.value
+	for _, child := range n.children {
+		sum += child.querySum()
+	}
+	return sum
+}
+
+func (n *Node) updateValues(w int) {
+	for n.parent != nil {
+		n.value = n.parent.value
+		n = n.parent
+	}
+	n.value = w
+}
+
+func buildTree(values []int, edges [][]int) *Node {
+	nodes := make([]*Node, len(values)+1)
+	for i, value := range values {
+		nodes[i+1] = newNode(value)
+	}
+
+	for _, edge := range edges {
+		u, v := edge[0], edge[1]
+		nodes[u].children = append(nodes[u].children, nodes[v])
+		nodes[v].parent = nodes[u]
+	}
+
+	return nodes[1]
+}
+
+func solution(values []int, edges [][]int, queries [][]int) []int {
+	tree := buildTree(values, edges)
+	result := make([]int, 0)
+
+	for _, query := range queries {
+		u, w := query[0], query[1]
+
+		if w == -1 {
+			result = append(result, tree.children[u-2].querySum())
 		} else {
-			sum -= sequence[i]
-			i++
+			if u == 1 {
+				tree.updateValues(w)
+			} else {
+				tree.children[u-2].updateValues(w)
+			}
 		}
 	}
-	return ans
+
+	return result
 }
 
 func main() {
-	sequence := []int{1, 2, 3, 4, 5, 7, 9, 10, 13, 15, 20}
-	k := 23
-	ans := solution(sequence, k)
-	fmt.Println(ans)
+	values := []int{1, 10, 100, 1000, 10000}
+	edges := [][]int{{1, 2}, {1, 3}, {2, 4}, {2, 5}}
+	queries := [][]int{{1, -1}, {2, -1}, {3, -1}, {4, -1}, {5, -1}, {4, 1000}, {1, -1}, {2, -1}, {3, -1}, {4, -1}, {5, -1}, {2, 1}, {1, -1}, {2, -1}, {3, -1}, {4, -1}, {5, -1}}
+
+	result := solution(values, edges, queries)
+	fmt.Println(result)
 }
